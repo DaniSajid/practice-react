5bee10705a525cd89532cd9d28eb1fc769d0cddf
DaniyalSajid <nani.playz49@gmail.com>
2024-05-01 12:23:48 -0700

    thirdCommit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import { HashRouter,Routes,Route } from 'react-router-dom'
 import './App.css'
 
 import Footer from './Footer/Footer'
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <>
-    <BrowserRouter>
+    <HashRouter>
       <Navbar/>
       <Routes>
       <Route path='/' element={<Home/>} />
@@ -21,7 +21,7 @@ function App() {
       <Route path='service' element={<Service/>} />
       </Routes>
       <Footer/>
-      </BrowserRouter>
+      </HashRouter>
     </>
   )
 }
